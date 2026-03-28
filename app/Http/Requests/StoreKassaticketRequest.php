670b8bfb1beefbe85de597477a854d8352e9198c
Robin Vermeir <robin.v.vermeir@gmail.com>
2026-03-28 20:48:31 +0100

    Breeze Authentication toegevoegd, StoreKassaticketRequest die de validatie uitvoert wanneer er een nieuw kassaticket aangemaakt moet worden

---
+++ app/Http/Requests/StoreKassaticketRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreKassaticketRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'klant' => 'required|max:100',
+            'email' => 'required|email',
+            'ticket_path' => 'required|mimes:png,jpeg,jpg,pdf|max:2048'
+        ];
+    }
+}

7fcfe2785f2b858aede5311e9e624d2f0ea93775
Robin Vermeir <robin.v.vermeir@gmail.com>
2026-03-28 22:03:28 +0100

    Bootstrap aanpassingen

--- resources/views/toevoeging_kassaticket.blade.php
+++ resources/views/toevoeging_kassaticket.blade.php
@@ -8,8 +8,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
@@ -24,38 +25,44 @@
         </div>
     @endif
 
-    Kassaticket toevoegen
+    <h1>Voeg uw kassaticket toe!</h1>
 
-    <form action="{{ route('kassaticket.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-3">
-            <label for="klant" class="form-label">Naam</label>
-            <input type="text" class="form-control" id="klant" name="klant" placeholder="Uw naam"
-                value="{{ old('klant') }}">
-            @error('klant')
-                <div style="color:red">{{ $message }}</div>
-            @enderror
+    <div class="container justify-center">
+        <div class="row">
+            <div class="col col-lg-8 mx-lg-auto">
+                <form action="{{ route('kassaticket.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="klant" class="form-label">Naam</label>
+                            <input type="text" class="form-control" id="klant" name="klant" placeholder="Uw naam"
+                                value="{{ old('klant') }}">
+                            @error('klant')
+                                <div style="color:red">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="naam@voorbeeld.com"
+                                value="{{ old('email') }}">
+                            @error('email')
+                                <div style="color:red">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="ticket_path" class="form-label">Foto van uw kassaticket</label>
+                            <input class="form-control" type="file" id="ticket_path" name="ticket_path"
+                                value="{{ old('ticket_path') }}">
+                            @error('ticket_path')
+                                <div style="color:red">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="col-12">
+                            <button class="btn btn-primary" type="submit">Submit form</button>
+                        </div>
+                </form>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="naam@voorbeeld.com"
-                value="{{ old('email') }}">
-            @error('email')
-                <div style="color:red">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="ticket_path" class="form-label">Foto van uw kassaticket</label>
-            <input class="form-control" type="file" id="ticket_path" name="ticket_path"
-                value="{{ old('ticket_path') }}">
-            @error('ticket_path')
-                <div style="color:red">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="col-12">
-            <button class="btn btn-primary" type="submit">Submit form</button>
-        </div>
-    </form>
+    </div>
 
 </body>
 

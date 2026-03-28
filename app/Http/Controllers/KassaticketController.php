f2812322e6daaa84374a1e07a4c6b67ec2ab8cf5
Robin Vermeir <robin.v.vermeir@gmail.com>
2026-03-28 22:30:21 +0100

    CSS aanpassingen, images worden in het project opgeslagen

--- app/Http/Controllers/KassaticketController.php
+++ app/Http/Controllers/KassaticketController.php
@@ -4,65 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreKassaticketRequest;
 use App\Models\Kassaticket;
+use Illuminate\Container\Attributes\Storage;
 use Illuminate\Http\Request;
 
 class KassaticketController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         return view('toevoeging_kassaticket');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreKassaticketRequest $request)
     {
-        $data = Kassaticket::create($request->validated());
+        $data = $request->validated();
+
+        $path = $request->ticket_path->store('images');
+
+        Kassaticket::create([
+            'klant' => $data['klant'],
+            'email' => $data['email'],
+            'ticket_path' => $path
+        ]);
 
         return redirect()->route('kassaticket.index')->with('success', 'Kassaticket aangemaakt!');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Kassaticket $kassaticket)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Kassaticket $kassaticket)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Kassaticket $kassaticket)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Kassaticket $kassaticket)
-    {
-        //
-    }
 }
--- resources/views/toevoeging_kassaticket.blade.php
+++ resources/views/toevoeging_kassaticket.blade.php
@@ -9,8 +9,10 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
 
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
@@ -18,49 +20,48 @@
     @endif
 </head>
 
-<body class="">
-    @if (session('success'))
-        <div class="alert alert-success" role="alert">
-            {{ session('success') }}
-        </div>
-    @endif
+<body class="mx-5">
 
-    <h1>Voeg uw kassaticket toe!</h1>
+    <h1 class="text-center my-5">Voeg uw kassaticket toe!</h1>
 
     <div class="container justify-center">
-        <div class="row">
-            <div class="col col-lg-8 mx-lg-auto">
-                <form action="{{ route('kassaticket.store') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="klant" class="form-label">Naam</label>
-                            <input type="text" class="form-control" id="klant" name="klant" placeholder="Uw naam"
-                                value="{{ old('klant') }}">
-                            @error('klant')
-                                <div style="color:red">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="naam@voorbeeld.com"
-                                value="{{ old('email') }}">
-                            @error('email')
-                                <div style="color:red">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="ticket_path" class="form-label">Foto van uw kassaticket</label>
-                            <input class="form-control" type="file" id="ticket_path" name="ticket_path"
-                                value="{{ old('ticket_path') }}">
-                            @error('ticket_path')
-                                <div style="color:red">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="col-12">
-                            <button class="btn btn-primary" type="submit">Submit form</button>
-                        </div>
-                </form>
-            </div>
+
+        <div class="col col-lg-8 mx-lg-auto">
+            <form action="{{ route('kassaticket.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="mb-3">
+                    <label for="klant" class="form-label font-weight-bold">Naam</label>
+                    <input type="text" class="form-control" id="klant" name="klant" placeholder="Uw naam"
+                        value="{{ old('klant') }}">
+                    @error('klant')
+                        <div style="color:red">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label font-weight-bold">Email</label>
+                    <input type="email" class="form-control" id="email" name="email"
+                        placeholder="naam@voorbeeld.com" value="{{ old('email') }}">
+                    @error('email')
+                        <div style="color:red">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="ticket_path" class="form-label font-weight-bold">Foto van uw kassaticket</label>
+                    <input class="form-control" type="file" id="ticket_path" name="ticket_path"
+                        value="{{ old('ticket_path') }}">
+                    @error('ticket_path')
+                        <div style="color:red">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="col-12 mb-3">
+                    <button class="btn btn-success" type="submit">Verstuur</button>
+                </div>
+            </form>
+            @if (session('success'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('success') }}
+                </div>
+            @endif
         </div>
     </div>
 
